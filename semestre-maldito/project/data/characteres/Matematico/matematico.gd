1589dba22a6bcf9407aa528d4d08393eb0bc185c
SamuDevGit <samu.davargas@gmail.com>
2026-06-03 00:25:24 -0500

    Diseños/Interfaz para el mapa, mañana sigo, que me mueroo gente

---
+++ semestre-maldito/project/data/characteres/Matematico/matematico.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@export var animation: Node
+@export var area_2D: Node
+
+const speed = 200
+
+var ultima_direccion = "abajo"
+
+func _ready() -> void:
+	area_2D.body_entered.connect(_on_area_2d_body_entered)
+
+func _physics_process(delta: float) -> void:
+
+	velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("Arriba"):
+		velocity.y = -speed
+		ultima_direccion = "arriba"
+		animation.play("Caminar pa Arriba")
+
+	elif Input.is_action_pressed("Abajo"):
+		velocity.y = speed
+		ultima_direccion = "abajo"
+		animation.play("Caminar pa Abajo")
+
+	elif Input.is_action_pressed("Izquierda"):
+		velocity.x = -speed
+		ultima_direccion = "izquierda"
+		animation.play("Caminar pa la Izquierda")
+
+	elif Input.is_action_pressed("Derecha"):
+		velocity.x = speed
+		ultima_direccion = "derecha"
+		animation.play("Caminar pa la Derecha")
+
+	else:
+		match ultima_direccion:
+			"arriba":
+				animation.play("Idle Mirando Arriba")
+			"abajo":
+				animation.play("Idle Mirando Abajo")
+			"izquierda":
+				animation.play("Idle Mirando Izquierda")
+			"derecha":
+				animation.play("Idle Mirando Derecha")
+
+	move_and_slide()
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass
